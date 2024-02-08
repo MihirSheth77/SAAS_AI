19eb6bbe56069e04f5a2b5c7c28835b3de8a84f2
Mihir <sheth.mi@northeastern.edu>
2024-02-08 15:54:21 -0500

    Implemented basics

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+ 
+const DashboardPage = () =>{
+  return (
+    <div>
+    <p>Dashboard Page</p>
+    </div>
+  );
+}
+
+export default DashboardPage;
